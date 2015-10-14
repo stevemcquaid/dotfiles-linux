1194e783b6d51df70265a6cf36026df5b54eb07a
Stephen McQuaid <smcquaid@godaddy.com>
2015-10-14 15:01:47 -0700

    Added sublime + bug fix for right-click functionality

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 apt-get install -y xserver-xorg-input-mtrack
 apt-get install -y screen
 
+wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_i386.deb
+dpkg -i sublime-text_build-3083_i386.deb
+
 cp config_files/bash_profile $HOME/.bash_profile
 cp config_files/xorg.conf /etc/X11/xorg.conf
 
