241b28d0086a6226fea46414c0f1d50296c8ef64
Stephen McQuaid <smcquaid@godaddy.com>
2015-10-14 14:30:51 -0700

    Changed bootstrap and organized repo a bit

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@
 apt-get install -y xserver-xorg-input-mtrack
 apt-get install -y screen
 
+cp config_files/bash_profile $HOME/.bash_profile
+cp config_files/xorg.conf /etc/X11/xorg.conf
 
-cp xorg.conf /etc/X11/xorg.conf
 
 pkill X
